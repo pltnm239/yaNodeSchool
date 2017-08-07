943b298a28bd13df048c7d3f500da7454fa05196
Mihail Cherviakov <pltnm239@gmail.com>
2017-08-07 23:07:04 +0300

    phone validation done

--- index.js
+++ index.js
@@ -1,49 +1,64 @@
 class Form{
-    constructor(formId, arrWithInputNames){
+    constructor(formId, resultContainerId, arrWithInputNames){
+        this._form = document.querySelector(`form#${formId}`);
+        this._form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.submit(e);
+        });
         this._inputs = arrWithInputNames.map(el => {
-            const input = document.querySelector(`input[name="${el.name}"]`);
+            const input = this._form.querySelector(`input[name="${el.name}"]`);
             input.onchange = el.customValidity;
             return {
                 name: el.name,
                 input: input
             };
         });
-
-        this._form = document.querySelector(`form#${formId}`);
-        this._form.addEventListener('submit', e => {
-            e.preventDefault();
-            this.submit(e);
-        })
+        this._apiUrl = this._form.getAttribute('action');
+        this._submitButton = this._form.querySelector('button[type="submit"]');
+        this._resultContainer = document.querySelector(`#${resultContainerId}`);
     }
     validate(){
-        this._invalidInputs = this._inputs.filter(el => el.input.getAttribute('isValid') === 'false');
+        this._invalidInputs = this._inputs.filter(el => el.input.getAttribute('data-valid') === 'false');
         return {
             isValid: !this._invalidInputs.length,
             errorFields: this._invalidInputs.map(el => el.name) || null
         }
     }
-    setData(object){
-
+    getData(){
+        let data = {};
+        this._inputs.forEach(el => data[el.name] = el.input.value);
+        return data;
+    }
+    setData(data){
+        this._inputs.forEach(el => {
+            if(data[el.name]) el.input.value = data[el.name];
+        });
     }
     submit(e){
+        e.preventDefault();
         this._inputs.forEach(el => el.input.classList.remove('error'));
         if(!this.validate().isValid) {
             this._invalidInputs.forEach(el => el.input.classList.add('error'));
-            return;
+        }else{
+            this._submitButton.innerText('Отправлено');
+            this._submitButton.setAttribute('disabled', true);
+
+            fetch(this._apiUrl, {mode: 'no-cors'})
+                .then(res => {
+                    console.log(this._apiUrl);
+                    console.log(this._resultContainer, res);
+                })
+                .catch(err => console.log('Error:', err));
         }
-        this._form
-            .querySelector('#submitButton')
-            .setAttribute('disabled', true);
     }
 }
-
-window.MyForm = new Form('myForm',[
+window.MyForm = new Form('myForm', 'resultContainer', [
     {
         name: 'fio',
         customValidity: e => {
             const value = e.target.value;
             const fullNameRegExp = /^([a-zа-яё-\s]+|\d+)$/i;
-            e.target.setAttribute('isValid', fullNameRegExp.test(value) && value.split(' ').length === 3);
+            e.target.setAttribute('data-valid', fullNameRegExp.test(value) && value.split(' ').length === 3);
         }
     },
     {
@@ -52,19 +67,19 @@ window.MyForm = new Form('myForm',[
             const value = e.target.value;
             const emailRegExp = /^([a-z0-9_\.-]+)@(yandex)+\.(ru|ua|by|kz|com)$/;
             const emailRegExpSecond = /^([a-z0-9_\.-]+)@(ya)+\.(ru)$/;
-            e.target.setAttribute('isValid', emailRegExp.test(value) || emailRegExpSecond.test(value));
+            e.target.setAttribute('data-valid', emailRegExp.test(value) || emailRegExpSecond.test(value));
         }
     },
     {
         name: 'phone',
         customValidity: e => {
             const value = e.target.value;
-            value.split('').filter(el => typeof +el === 'number');
-
-            const phoneRegExp = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
-
-            e.target.setAttribute('isValid', true);
-
+            const phoneRegExp = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
+            let phoneNumbersSum = 0;
+            value.split('')
+                .filter(el => el.match(/\d/))
+                .forEach(el => phoneNumbersSum += +el);
+            e.target.setAttribute('data-valid', phoneNumbersSum <= 30 && phoneRegExp.test(value));
         }
     }
 ]);
