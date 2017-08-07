5cb8506932c30a81c6340c13e42509db7ef28005
pltnm239 <pltnm239@gmail.com>
2017-08-07 18:37:13 +0300

    add validation func

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 class Form{
-    constructor(arrWithInputNames){
+    constructor(formId, arrWithInputNames){
         this._inputs = arrWithInputNames.map(el => {
             const input = document.querySelector(`input[name="${el.name}"]`);
             input.onchange = el.customValidity;
-            return input;
+            return {
+                name: el.name,
+                input: input
+            };
         });
 
+        this._form = document.querySelector(`form#${formId}`);
+        this._form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.submit(e);
+        })
     }
     validate(){
-        const invalidInputs = this._inputs.filter(el => el.getAttribute('isValid') === 'false');
+        this._invalidInputs = this._inputs.filter(el => el.input.getAttribute('isValid') === 'false');
         return {
-            isValid: !invalidInputs.length,
-            errorFields: invalidInputs.map(el => el.name) || null
+            isValid: !this._invalidInputs.length,
+            errorFields: this._invalidInputs.map(el => el.name) || null
         }
     }
     setData(object){
 
     }
-    submit(){
-        if(this.validate().isValid){
-
+    submit(e){
+        this._inputs.forEach(el => el.input.classList.remove('error'));
+        if(!this.validate().isValid) {
+            this._invalidInputs.forEach(el => el.input.classList.add('error'));
+            return;
         }
+        this._form
+            .querySelector('#submitButton')
+            .setAttribute('disabled', true);
     }
 }
 
-window.MyForm = new Form([
+window.MyForm = new Form('myForm',[
     {
         name: 'fio',
         customValidity: e => {
-            e.target.setAttribute('isValid', true);
+            const value = e.target.value;
+            const fullNameRegExp = /^([a-zа-яё-\s]+|\d+)$/i;
+            e.target.setAttribute('isValid', fullNameRegExp.test(value) && value.split(' ').length === 3);
         }
     },
     {
         name: 'email',
         customValidity: e => {
-            e.target.setAttribute('isValid', true);
+            const value = e.target.value;
+            const emailRegExp = /^([a-z0-9_\.-]+)@(yandex)+\.(ru|ua|by|kz|com)$/;
+            const emailRegExpSecond = /^([a-z0-9_\.-]+)@(ya)+\.(ru)$/;
+            e.target.setAttribute('isValid', emailRegExp.test(value) || emailRegExpSecond.test(value));
         }
     },
     {
         name: 'phone',
         customValidity: e => {
+            const value = e.target.value;
+            const phoneRegExp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
             e.target.setAttribute('isValid', true);
+
         }
     }
 ]);
+
